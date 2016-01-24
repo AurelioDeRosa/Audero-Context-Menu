6271382f93643818207fd94fa1f958adf8e50569
Aurelio De Rosa <aurelioderosa@gmail.com>
2016-01-24 23:36:58 +0000

    Released version 2.4.2

--- src/js/jquery.auderoContextMenu.js
+++ src/js/jquery.auderoContextMenu.js
@@ -5,6 +5,7 @@
    if (typeof define === 'function' && define.amd) {
       define(['jquery'], factory);
    } else if (typeof module === 'object' && module.exports) {
+
       module.exports = function(root, jQuery) {
          if (jQuery === undefined) {
             jQuery = typeof window !== 'undefined' ? require('jquery') : require('jquery')(root);
